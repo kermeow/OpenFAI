c4e659532a1f7bf5c551c228a6f6013028914f61
jayden <46307433+krmeet@users.noreply.github.com>
2023-04-01 19:59:40 +0100

    flipped y

--- scripts/gameplay/Floor.gd
+++ scripts/gameplay/Floor.gd
@@ -10,8 +10,9 @@ class_name Floor
 func _ready():
 	line.set_point_position(0,Vector2(-length,0))
 	line.set_point_position(2,Vector2(
-		cos(deg_to_rad(angle)),sin(deg_to_rad(angle)))
-		*length)
+		cos(deg_to_rad(angle)),
+		-sin(deg_to_rad(angle))
+		)*length)
 	if previous_floor_path != null:
 		var previous_floor = get_node_or_null(previous_floor_path)
 		if previous_floor == self or previous_floor.previous_floor_path == self.get_path():
@@ -24,9 +25,9 @@ func align_to_floor(last_floor:Floor):
 	var last_angle = last_floor.angle
 	line.set_point_position(0,Vector2(
 		cos(deg_to_rad(last_angle)),
-		sin(deg_to_rad(last_angle))
+		-sin(deg_to_rad(last_angle))
 		)*-length)
 	position = last_floor.position + Vector2(
 		cos(deg_to_rad(last_angle)),
-		sin(deg_to_rad(last_angle))
+		-sin(deg_to_rad(last_angle))
 		)*(length + last_floor.length)
